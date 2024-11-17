d3bb250e4d38f7c8507d5d054891806627719dbe
KarolinaR98 <64532044+KarolinaR98@users.noreply.github.com>
2024-11-17 16:47:23 +0100

    github pages fixes update

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { CountriesProvider } from "./context/countriesContext.tsx";
 import { DarkModeProvider } from "./context/darkModeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <DarkModeProvider>
       <CountriesProvider>
       <App />
     </CountriesProvider>   
     </DarkModeProvider>       
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
